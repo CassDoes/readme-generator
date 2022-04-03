c1492717e240e44399b7c7479b9f654c42cf6a16
CassDoes <c.doexo@gmail.com>
2022-04-02 21:18:50 -0500

    added additional questions

--- app.js
+++ app.js
@@ -13,19 +13,59 @@
 //      init();
 
 
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
 
-//profileDataArgs = readmeDataArgs
-const readmeDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown.js');
 
-//profileDataArgs = readmeDataArgs
-const [data] = readmeDataArgs;
+// const pageData = generateMarkdown(data)
 
-
-
-fs.writeFile('./README.md', generateMarkdown(data), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./README.md', pageData, err => {
+//     if (err) throw err;
   
-    console.log('READme complete!');
-  });
+//     console.log('READme complete!');
+//   });
+
+
+//add validate prompts in 9.3.6
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a short description explaining the what, why, and how of your project.'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples for how your project should be used.'
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List your collaborators, if any, with links to their GitHub profiles.'
+        },
+        {
+            type: 'input',
+            name: 'licenses',
+            message: 'How are other developers allowed to use your project?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What is your GitHub user name and email? How can you be reached for additional questions?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What testing has your project been through?'
+        },
+    ]);
+};
+
+promptUser().then(answers => console.log(answers));
